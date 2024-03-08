43f4b660d5713400df962254cd68084ab9cd76ad
harshalgunjal69 <hrshlgunjal@gmail.com>
2024-03-08 16:01:40 +0530

    Update color scheme in globals.css and some transition in navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,43 +3,42 @@
 import { useState } from 'react';
 import { DarkModeToggle } from './DarkModeToggle';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { TextAlignJustifyIcon } from '@radix-ui/react-icons';
 
 function Navbar() {
     const [showMenu, setShowMenu] = useState(false);
+    const pathname = usePathname();
+    const NavLinks = [
+        { id: 1, name: 'Home', path: '/' },
+        { id: 2, name: 'About', path: '/about' },
+        { id: 3, name: 'Blogs', path: '/blogs' },
+        { id: 4, name: 'Contact', path: '/contact' },
+    ];
     return (
         <>
-            <div className="hidden lg:flex backdrop-filter backdrop-blur-3xl opacity-80 bg-background dark:bg-background text-black dark:text-white dark:border-gray-700 border-2 border-gray-200 w-[90vw] h-16 fixed top-0 z-50 items-center justify-between px-4 sm:px-8 lg:px-16 transition-all duration-300 ease-in-out shadow-md rounded-lg my-3">
+            <div className="hidden lg:flex backdrop-filter backdrop-blur-3xl opacity-80 bg-background dark:bg-background text-black dark:text-white dark:border-gray-700 border-2 border-gray-200 w-[90vw] h-16 fixed top-0 z-50 items-center justify-between px-4 sm:px-8 transition-all duration-300 ease-in-out shadow-md rounded-lg my-3">
                 <div className="flex items-center space-x-4 w-1/3 text-start">
                     <h1 className="font-bold text-2xl text-black dark:text-white">
                         Real Estate
                     </h1>
                 </div>
                 <div className="flex items-center space-x-8">
-                    <Link
-                        href="/"
-                        className="font-semibold text-md hover:scale-125 transition-all duration-175 ease-[cubic-bezier(0.95,0.05,0.795,0.035)]"
-                    >
-                        Home
-                    </Link>
-                    <Link
-                        href="/about"
-                        className="font-semibold text-md hover:scale-125 transition-all duration-175 ease-[cubic-bezier(0.95,0.05,0.795,0.035)]"
-                    >
-                        About
-                    </Link>
-                    <Link
-                        href="/contact"
-                        className="font-semibold text-md hover:scale-125 transition-all duration-175 ease-[cubic-bezier(0.95,0.05,0.795,0.035)]"
-                    >
-                        Contact
-                    </Link>
-                    <Link
-                        href="/blogs"
-                        className="font-semibold text-md hover:scale-125 transition-all duration-175 ease-[cubic-bezier(0.95,0.05,0.795,0.035)]"
-                    >
-                        Blogs
-                    </Link>
+                    {NavLinks.map((link) => {
+                        return (
+                            <Link
+                                key={link.id}
+                                href={link.path}
+                                className={`text-md ${
+                                    pathname === link.path
+                                        ? 'text-black dark:text-white font-bold'
+                                        : 'text-gray-600 dark:text-gray-400 font-semibold'
+                                } hover:text-gray-900 dark:hover:text-gray-200 transition-all duration-150 ease-in-out`}
+                            >
+                                {link.name}
+                            </Link>
+                        );
+                    })}
                 </div>
                 <div className=" w-1/3 text-end">
                     <DarkModeToggle />
@@ -60,31 +59,22 @@ function Navbar() {
                 </div>
 
                 {showMenu && (
-                    <div className="absolute top-16 left-0 w-full bg-white dark:bg-[#020816] dark:text-white dark:border-gray-700 border-b-2 border-gray-200 p-4 shadow-md origin-top animate-open-menu">
-                        <Link
-                            href="/"
-                            className="block font-semibold text-lg py-2 text-center"
-                        >
-                            Home
-                        </Link>
-                        <Link
-                            href="/about"
-                            className="block font-semibold text-lg py-2 text-center"
-                        >
-                            About
-                        </Link>
-                        <Link
-                            href="/blogs"
-                            className="block font-semibold text-lg py-2 text-center"
-                        >
-                            Blogs
-                        </Link>
-                        <Link
-                            href="/contact"
-                            className="block font-semibold text-lg py-2 text-center"
-                        >
-                            Contact
-                        </Link>
+                    <div className="absolute top-16 left-0 w-full bg-background dark:bg-background dark:text-white dark:border-gray-700 border-b-2 border-gray-200 p-4 shadow-md origin-top animate-open-menu">
+                        {NavLinks.map((link) => {
+                            return (
+                                <Link
+                                    key={link.id}
+                                    href={link.path}
+                                    className={`block font-semibold ${
+                                        pathname === link.path
+                                            ? 'text-black dark:text-white'
+                                            : 'text-gray-600 dark:text-gray-400'
+                                    } text-lg py-2 text-center`}
+                                >
+                                    {link.name}
+                                </Link>
+                            );
+                        })}
                     </div>
                 )}
             </div>
